c85395b970d7b764abdc51a4b0b5296ba74e542a
default-007 <brianokola@gmail.com>
2020-06-06 13:26:39 +0300

    Add functionality to the signup for in the signup method in view

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'crispy_forms',
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -121,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'index' 
--- myawards/views.py
+++ myawards/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import  SignupForm
+from django.contrib.auth import login, authenticate
 
 
 # Create your views here.
@@ -8,5 +9,15 @@ def index(request):
 
 
 def signup(request):
-    form = SignupForm
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
