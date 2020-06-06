de3ffb6992b80a8880de1275cb58cb7203ecec29
default-007 <brianokola@gmail.com>
2020-06-06 13:15:53 +0300

    Create template to render hello

--- awwwards/settings.py
+++ awwwards/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import dj_database_url
+from decouple import config, Csv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myawards',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
---
+++ myawards/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+]
