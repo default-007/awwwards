c4edad8fbccf99024fe85b2886989d6dccc4a73b
default-007 <brianokola@gmail.com>
2020-06-07 12:42:12 +0300

    Update my Model functionsto include search function and date model

--- myawards/models.py
+++ myawards/models.py
@@ -31,13 +31,30 @@ class Post(models.Model):
     title = models.CharField(max_length=155)
     url = models.URLField(max_length=255)
     description = models.TextField(max_length=255)
-    technologies = models.CharField(max_length=200)
+    technologies = models.CharField(max_length=200, blank=True)
     photo = ImageField(manual_crop='1280x720')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
         return f'{self.title}'
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()
+
+
+
 
 class Rating(models.Model):
     rating = (
@@ -63,7 +80,7 @@ class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='rater')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings', null=True)
 
-    def save_comment(self):
+    def save_rating(self):
         self.save()
 
     @classmethod
