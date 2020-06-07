4b52c783394e3239e6849956889bca43fafa8f00
default-007 <brianokola@gmail.com>
2020-06-07 12:33:59 +0300

    REgister the post api view set

--- myawards/serializers.py
+++ myawards/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.models import User
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'url', 'username']
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['id', 'user', 'profile_picture', 'bio', 'name', 'location', 'contact']
+        fields = ['name', 'profile_picture', 'bio', 'location', 'contact']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'url', 'description', 'technologies', 'photo', 'date', 'user']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    posts = PostSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'url', 'username', 'profile', 'posts']
--- myawards/urls.py
+++ myawards/urls.py
@@ -3,16 +3,19 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('profile', views.ProfileViewSet)
 router.register('users', views.UserViewSet)
+router.register('posts', views.PostViewSet)
+router.register('profile', views.ProfileViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('account/', include('django.contrib.auth.urls')),
     path('api/', include(router.urls)),
+    path('<username>/profile', views.user_profile, name='userprofile'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('profile/<username>/', views.profile, name='profile'),
     path('profile/<username>/settings', views.edit_profile, name='edit'),
     path('project/<post>', views.project, name='project'),
+    path('search/', views.search_project, name='search'),
 ]
