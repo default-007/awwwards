e01dcc848d8f3ed869a0165be012ff1b1261a7c3
default-007 <brianokola@gmail.com>
2020-06-07 12:16:54 +0300

    Added search project functionality for user to search for projects

--- myawards/views.py
+++ myawards/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import SignupForm, PostForm, UpdateUserForm, UpdateUserProfileForm, RatingsForm
 from rest_framework import viewsets
 from .models import Profile, Post, Rating
@@ -7,6 +7,7 @@ from .serializers import ProfileSerializer, UserSerializer
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+import random
 
 
 def index(request):
@@ -14,17 +15,20 @@ def index(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.user = request.user.profile
+            post.user = request.user
             post.save()
     else:
         form = PostForm()
 
     try:
         posts = Post.objects.all()
-        print(posts)
+        posts = posts[::-1]
+        a_post = random.randint(0, len(posts)-1)
+        random_post = posts[a_post]
+        print(random_post)
     except Post.DoesNotExist:
         posts = None
-    return render(request, 'index.html', {'posts': posts, 'form': form})
+    return render(request, 'index.html', {'posts': posts, 'form': form, 'random_post': random_post})
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
@@ -51,9 +55,21 @@ def signup(request):
         form = SignupForm()
     return render(request, 'registration/signup.html', {'form': form})
 
+
 @login_required(login_url='login')
 def profile(request, username):
     return render(request, 'profile.html')
+
+
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('profile', username=request.user.username)
+    params = {
+        'user_prof': user_prof,
+    }
+    return render(request, 'userprofile.html', params)
+
 
 @login_required(login_url='login')
 def edit_profile(request, username):
@@ -74,8 +90,16 @@ def edit_profile(request, username):
     }
     return render(request, 'edit.html', params)
 
+
+@login_required(login_url='login')
 def project(request, post):
     post = Post.objects.get(title=post)
+    ratings = Rating.objects.filter(user=request.user, post=post).first()
+    rating_status = None
+    if ratings is None:
+        rating_status = False
+    else:
+        rating_status = True
     if request.method == 'POST':
         form = RatingsForm(request.POST)
         if form.is_valid():
@@ -86,26 +110,45 @@ def project(request, post):
             post_ratings = Rating.objects.filter(post=post)
 
             design_ratings = [d.design for d in post_ratings]
-            design_average = sum(design_ratings)/len(design_ratings)
+            design_average = sum(design_ratings) / len(design_ratings)
 
             usability_ratings = [us.usability for us in post_ratings]
-            usability_average = sum(usability_ratings)/len(usability_ratings)
+            usability_average = sum(usability_ratings) / len(usability_ratings)
 
             content_ratings = [content.content for content in post_ratings]
-            content_average = sum(content_ratings)/len(content_ratings)
+            content_average = sum(content_ratings) / len(content_ratings)
 
-            score = (design_average + usability_average + content_average)/3
+            score = (design_average + usability_average + content_average) / 3
             print(score)
             rate.design_average = round(design_average, 2)
             rate.usability_average = round(usability_average, 2)
             rate.content_average = round(content_average, 2)
             rate.score = round(score, 2)
             rate.save()
+            return HttpResponseRedirect(request.path_info)
     else:
         form = RatingsForm()
     params = {
         'post': post,
-        'rating_form': form
+        'rating_form': form,
+        'rating_status': rating_status
 
     }
     return render(request, 'project.html', params)
+
+
+def search_project(request):
+    if request.method == 'GET':
+        title = request.GET.get("title")
+        results = Post.objects.filter(title__icontains=title).all()
+        print(results)
+        message = f'name'
+        params = {
+            'results': results,
+            'message': message
+        }
+        return render(request, 'results.html', params)
+    else:
+        message = "You haven't searched for any image category"
+    return render(request, 'results.html', {'message': message})
+    
